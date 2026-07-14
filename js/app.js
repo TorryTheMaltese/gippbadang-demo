7ed3572042de94b85f3b76807ad86c8f7e338982
Torry <torryiscute@gmail.com>
2026-07-14 20:05:45 +0900

    edit typo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { initChat } from './chat.js';
 import { initDrive } from './drive.js';
 import { initClassroom } from './classroom.js';
-import { initPlugins } from './plugins.js';
+import { initPlugins } from './plugin.js';
 
 export const $ = (selector, root = document) => root.querySelector(selector);
 export const $$ = (selector, root = document) => [...root.querySelectorAll(selector)];
